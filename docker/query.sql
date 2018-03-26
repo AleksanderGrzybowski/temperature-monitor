0d2e4871145837c899ca5692f5f5229234580d4a
Aleksander Grzybowski <kelog@orange.pl>
2018-03-26 19:16:51 +0200

    Sample working Grafana SQL query

---
+++ docker/query.sql
@@ -0,0 +1,9 @@
+# noinspection SqlResolveForFile
+
+SELECT
+  UNIX_TIMESTAMP(date_time) as time_sec,
+  temperature as 'value',
+  place as metric
+FROM temperature_info
+WHERE $__timeFilter(date_time)
+ORDER BY date_time ASC
