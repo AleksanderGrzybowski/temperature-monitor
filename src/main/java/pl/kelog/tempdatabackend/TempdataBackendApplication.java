02e591dfa8ffb9bf916ba7f3bef6801ab2887d85
Aleksander Grzybowski <kelog@orange.pl>
2018-03-26 17:56:38 +0200

    Empty Spring Boot app

---
+++ src/main/java/pl/kelog/tempdatabackend/TempdataBackendApplication.java
@@ -0,0 +1,12 @@
+package pl.kelog.tempdatabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempdataBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempdataBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/kelog/tempdatabackend/TempdataBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.kelog.tempdatabackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TempdataBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
